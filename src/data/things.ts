40e55dc5db44f0c26ba970f850000da80747c16a
Sergio <sergiot.fontal@gmail.com>
2023-02-16 20:42:10 +0100

    Create things list

---
+++ src/data/things.ts
@@ -0,0 +1,16 @@
+const things = {
+  things: [
+    {
+      id: 1,
+      name: "Declare a variable",
+    },
+    {
+      id: 2,
+      name: "Use console.log() method",
+    },
+    {
+      id: 3,
+      name: "Iterate an array",
+    },
+  ],
+};
