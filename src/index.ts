be33b6d01152530fa58bf9be5e0a5cbeb8e4f83d
Sergio <sergiot.fontal@gmail.com>
2023-02-16 18:56:10 +0100

    Add listen requests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import "./loadEnvirontment.ts";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  console.log(`Server is listening requests from ${port} port.`);
+});
