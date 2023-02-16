9f5f43bb2f7ef2cd4d75468afb841b96b35a584c
Sergio <sergiot.fontal@gmail.com>
2023-02-16 22:12:54 +0100

    Add morgan middleware

--- src/controllers/thingsControllers.ts
+++ src/controllers/thingsControllers.ts
@@ -2,6 +2,5 @@ import type { Request, Response } from "express";
 import { things } from "../data/things.js";
 
 export const getThings = (req: Request, res: Response) => {
-  console.log("A GET request has arrive to /things");
   res.status(200).json(things);
 };
