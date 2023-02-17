613c4c7880574e624733c3e94b608ea584350d78
Sergio <sergiot.fontal@gmail.com>
2023-02-17 07:14:15 +0100

    Add getThing controller and its router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./loadEnvirontment.js";
 import express from "express";
-import { getThings } from "./controllers/thingsControllers.js";
+import { getThing, getThings } from "./controllers/thingsControllers.js";
 import morgan from "morgan";
 import thingsRouter from "./routers/thingsRouters.js";
 
@@ -11,8 +11,10 @@ app.use(morgan("dev"));
 
 app.use("/things", thingsRouter);
 
+app.get("/", getThings);
+
+app.get("/:id", getThing);
+
 app.listen(port, () => {
   console.log(`Server is listening requests from ${port} port.`);
 });
-
-app.get("/", getThings);
