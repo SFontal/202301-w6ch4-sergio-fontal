be31002beda820e28677146d3aa744fa70d4eb66
Sergio <sergiot.fontal@gmail.com>
2023-02-17 08:01:23 +0100

    Add createThing controller and its router

--- src/controllers/thingsControllers.ts
+++ src/controllers/thingsControllers.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-implicit-coercion */
 import type { Request, Response } from "express";
 import { things } from "../data/things.js";
+import { type ThingStructure } from "../types.js";
 
 export const getThings = (req: Request, res: Response) => {
   res.status(200).json(things);
@@ -18,4 +19,19 @@ export const deleteThing = (req: Request, res: Response) => {
   const newThings = things.things.filter((thing) => thing.id !== +id);
 
   res.status(200).json(newThings);
+};
+
+export const createThing = (
+  req: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    ThingStructure
+  >,
+  res: Response
+) => {
+  const newThing = req.body;
+
+  things.things.push({ ...newThing, id: Date.now() });
+
+  res.status(200).json(things);
 };
